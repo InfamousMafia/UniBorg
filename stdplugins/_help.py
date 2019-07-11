f1ebf1b946dc34cdb0e0111706830a73aae80b96
InfamousMafia <50261821+InfamousMafia@users.noreply.github.com>
2019-07-12 00:41:33 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -16,7 +16,7 @@ async def _(event):
 Python {}
 Telethon {}
 
-**Custom Built Fork**: https://github.com/ravana69/UniBorg""".format(
+**Fork**: https://github.com/zeususerbot/UniBorg""".format(
         sys.version,
         __version__
     )
